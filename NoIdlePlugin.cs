93bd306834008b90619db54d038111ba68524db5
LordTylus <LordTylus@users.noreply.github.com>
2020-03-24 00:26:24 +0100

    updated so it doesnt use torch internals its not supposed to

--- NoIdlePlugin.cs
+++ NoIdlePlugin.cs
@@ -1,24 +1,13 @@
-﻿using NLog;
-using Torch;
+﻿using Torch;
 using Torch.API;
 
 namespace ALE_NoIdle {
 
     public class NoIdlePlugin : TorchPluginBase
     {
-
-        public static NoIdlePlugin Instance { get; private set; }
-        public static readonly Logger Log = LogManager.GetCurrentClassLogger();
-
-        /// <inheritdoc />
         public override void Init(ITorchBase torch)
         {
             base.Init(torch);
-
-            var pgmr = new NoIdleManager(torch);
-            torch.Managers.AddManager(pgmr);
-
-            Instance = this;
         }
     }
 }
--- Patch/MyLargeTurretBasePatch.cs
+++ Patch/MyLargeTurretBasePatch.cs
@@ -3,19 +3,35 @@ using Sandbox.Game.Weapons;
 using System;
 using System.Reflection;
 using Sandbox.ModAPI;
+using Torch.Managers.PatchManager;
 
 namespace ALE_NoIdle {
-    class MyLargeTurretBasePatch {
+    
+    [PatchShim]
+    public static class MyLargeTurretBasePatch {
 
         public static readonly Logger Log = LogManager.GetCurrentClassLogger();
 
-        internal static readonly MethodInfo update =
+        internal static readonly MethodInfo UpdateMethod =
             typeof(MyLargeTurretBase).GetMethod(nameof(MyLargeTurretBase.UpdateAfterSimulation10), BindingFlags.Instance | BindingFlags.Public) ??
             throw new Exception("Failed to find patch method");
 
-        internal static readonly MethodInfo idleMover =
+        internal static readonly MethodInfo IdleMoverPatch =
             typeof(MyLargeTurretBasePatch).GetMethod(nameof(IdleMover), BindingFlags.Static | BindingFlags.Public) ??
             throw new Exception("Failed to find patch method");
+
+        public static void Patch(PatchContext ctx) {
+
+            try {
+
+                ctx.GetPattern(UpdateMethod).Prefixes.Add(IdleMoverPatch);
+
+                Log.Info("Patching Successful MyLargeTurretBase!");
+
+            } catch (Exception e) {
+                Log.Error(e, "Patching failed!");
+            }
+        }
 
         public static bool IdleMover(MyLargeTurretBase __instance) {
 
