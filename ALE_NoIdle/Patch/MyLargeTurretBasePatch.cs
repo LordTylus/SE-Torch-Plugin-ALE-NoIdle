15470a80b3feea368989c04d2d9d4725825b6c8c
LordTylus <LordTylus@users.noreply.github.com>
2020-08-08 00:04:29 +0200

    Compatibility fix

--- ALE_NoIdle/Patch/MyLargeTurretBasePatch.cs
+++ ALE_NoIdle/Patch/MyLargeTurretBasePatch.cs
@@ -4,6 +4,7 @@ using System;
 using System.Reflection;
 using Sandbox.ModAPI;
 using Torch.Managers.PatchManager;
+using VRage.Game.ModAPI;
 
 namespace ALE_NoIdle {
     
@@ -13,7 +14,7 @@ namespace ALE_NoIdle {
         public static readonly Logger Log = LogManager.GetCurrentClassLogger();
 
         internal static readonly MethodInfo UpdateMethod =
-            typeof(MyLargeTurretBase).GetMethod(nameof(MyLargeTurretBase.UpdateAfterSimulation10), BindingFlags.Instance | BindingFlags.Public) ??
+            typeof(MyLargeTurretBase).GetMethod(nameof(MyLargeTurretBase.UpdateAfterSimulation), BindingFlags.Instance | BindingFlags.Public) ??
             throw new Exception("Failed to find patch method");
 
         internal static readonly MethodInfo IdleMoverPatch =
@@ -27,16 +28,17 @@ namespace ALE_NoIdle {
             Log.Debug("Patching Successful MyLargeTurretBase!");
         }
 
-        public static bool IdleMover(MyLargeTurretBase __instance) {
-
-            IMyLargeTurretBase turret = __instance as IMyLargeTurretBase;
+        public static void IdleMover(MyLargeTurretBase __instance) {
+          
+            if (!(__instance is IMyLargeTurretBase turret))
+                return;
 
             if (turret.EnableIdleRotation) {
                 turret.EnableIdleRotation = false;
                 turret.SyncEnableIdleRotation();
             }
 
-            return true;
+            return;
         }
     }
 }
